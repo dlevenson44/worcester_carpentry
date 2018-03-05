8c7210f9299847b301d152e7281768363235d0ea
Daniel Levenson <daniellevenson4@gmail.com>
2018-03-05 14:24:17 -0500

    added descriptions to gutters and windowsdoors components

--- src/components/Gutters.jsx
+++ src/components/Gutters.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Gutters = () => {
     return(
         <div>
-            <h1>hey what's up</h1>
+            <h1>Gutters</h1>
+            <p>We install gutters and downspouts on all types of homes.  We work with our customers to find the best solution for water diversion.  We also install a range of gutter colors and materials.</p>
         </div>
     )
 }
--- src/components/WindowsDoors.jsx
+++ src/components/WindowsDoors.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 const WindowsDoors = () => {
     return(
         <div>
-            <h1>hey what's up</h1>
+            <h1>Windows and Doors</h1>
+            <p>We install replacement windows and new contruction windows depending on the specific application.  We work with many manufacturers of windows, as well as many different styles of windows.  We have experience in installing doors of all types and styles also.  Doors we install include:</p>
+            <ul>
+            	<li>Entry Doors</li>
+            	<li>Entry Doors with Sidelights</li>
+            	<li>Sliding Patio Doors</li>
+            	<li>French Doors</li>
+            	<li>All Types of Interior Doors</li>
+            </ul>
         </div>
     )
 }
