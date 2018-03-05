9abfaaf22ece30811be0b9625d5985b50659eefc
Daniel Levenson <daniellevenson4@gmail.com>
2018-03-05 13:49:28 -0500

    Set up structures for roofing, siding, and windowsdoor components

---
+++ src/components/Roofing.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Roofing = () => {
+    return(
+        <div>
+            <h1>hey what's up</h1>
+        </div>
+    )
+}
+
+export default Roofing
---
+++ src/components/WindowsDoors.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WindowsDoors = () => {
+    return(
+        <div>
+            <h1>hey what's up</h1>
+        </div>
+    )
+}
+
+export default WindowsDoors
