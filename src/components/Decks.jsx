75befdaa0ae774309c64a8a321dcdd35fb358700
Daniel Levenson <daniellevenson4@gmail.com>
2018-03-06 15:44:07 -0500

    added div for slideshow, photos to be used for decks component

--- src/components/Decks.jsx
+++ src/components/Decks.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 
+// use 1 image as banner, 3 for side by side slideshow
+// banner-- decksporches4
+// slideshow-- customdeckstairs1, decksporches7, deckporches8
+
 const Decks = () => {
     return(
         <div>
+            <div className="info">
             <h1>Decking</h1>
-            <p>We build and re-surface decks and porches.  Decks and porches can be as elaborate or as economical as needed.  We work with both wood and composite materials, as well as a combination of the two.  All decks and porches we build or repair are built or brought back to code regulations.</p>
+            <p>We build and re-surface decks and porches.</p>
+            <p>Decks and porches can be as elaborate or as economical as needed.</p>
+            <p>We work with both wood and composite materials, as well as a combination of the two.</p>
+            <p>All decks and porches we build or repair are built or brought back to code regulations.</p>
+            </div>
+            <div className="img">
+            </div>
         </div>
     )
 }
