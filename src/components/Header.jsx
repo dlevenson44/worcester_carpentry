c3a2202fdf6e284202a78704029f60b702099e54
Daniel Levenson <daniellevenson4@gmail.com>
2018-03-06 17:02:17 -0500

    set layout for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,18 @@ import React from 'react'
 
 const Header = () => {
     return(
-        <h1>hello</h1>
+        <div className="header-container">
+        <h1 className="header">hello</h1>
+        <div className="header-subcont">
+        <ul>
+            <li><a href="tel:508-523-6797">508-523-6797</a></li>
+            <li>Worcester County and the surrounding areas</li>
+            <li>Need masonry work done?  <a href="http://worcesterchimneys.com/" target="_blank">Click here!</a></li>
+        </ul>
+        </div>
+        </div>
     )
 }
 
 export default Header;
+
