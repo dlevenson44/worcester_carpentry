5dc6ab752f53d2cf55a704349f91f6fd83faa05a
Daniel Levenson <daniellevenson4@gmail.com>
2018-03-06 16:40:32 -0500

    renaimed container div class

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <div className="App">
+        <div className="container">
           <Header />
           <Nav />
           <Route path='/contact' component={Contact} />
