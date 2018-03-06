191fc33258f3eded31d97a013f56594409eec49a
Daniel Levenson <daniellevenson4@gmail.com>
2018-03-06 16:12:14 -0500

    selected images, redid structure for remodeling

--- src/components/Remodeling.jsx
+++ src/components/Remodeling.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 
+// 7 as banner
+// 1,4,9 as slideshow
+
+// 11 for home slideshow
+
 const Remodeling = () => {
     return(
         <div>
             <h1>Remodeling</h1>
-            <p>We are experienced in all phases of home remodeling, including interior, exterior, basements, kitchens, and bathrooms.  We work with our customers to help find the most practical solutions to their remodeling projects.</p>
+            <div className="info">
+            <p>We are experienced in all phases of home remodeling, including:</p>
+            <ul>
+                <li>Interior</li>
+                <li>Exterior</li>
+                <li>Basements</li>
+                <li>Kitchens</li>
+                <li>Bathrooms</li>
+            </ul>
+            </div>
+            <div className="img">
+            </div>
+            <p>We work with our customers to help find the most practical solutions to their remodeling projects.</p>
         </div>
     )
 }
