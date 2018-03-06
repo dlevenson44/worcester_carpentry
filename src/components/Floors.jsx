69397f696a16827b6f86fe7418914fae17537d3b
Daniel Levenson <daniellevenson4@gmail.com>
2018-03-06 18:03:05 -0500

    setup half of decking styling

--- src/components/Floors.jsx
+++ src/components/Floors.jsx
@@ -1,26 +1,34 @@
 import React from 'react'
-
-// banner img-- flooring 4
-// standalone img-- flooring 2
-// slideshow imgs--flooring 3, 1, 6
+import Slideshow1 from './imgs/flooring/flooring3.jpg'
+import Slideshow2 from './imgs/flooring/flooring1.jpg'
+import Slideshow3 from './imgs/flooring/flooring6.jpg'
+import Standalone from './imgs/flooring/flooring4.jpg'
 
 const Floors = () => {
     return(
-        <div>        
+        <div>           
             <h1>Flooring</h1>
+            <div className="flooring-container"> 
+            <div className="flooring-subcont">
             <div className="info">
-            <p>We at Worcester County Carpentry offer a wide range of flooring options.  We install many flooring choices.  Our tile options are not limited to floors only, as we also tile:</p>
+            <p>We at Worcester County Carpentry offer a wide range of flooring options.  We install many flooring choices.<br/>  Our tile options are not limited to floors only, as we also tile:</p>
+            <div className="sub">
             <ul>
                 <li>Walls</li>
                 <li>Shower stalls</li>
                 <li>Backsplashes</li>
                 <li>Ceilings</li>
-            </ul>     
-            </div>
+            </ul>                 
             <div className="img">
-            </div>       
+                <img src={Standalone} id="standalone" />
+            </div>
+            </div>
+            </div>
+            </div>
+            <div className="flooring-subcont">      
             <div className="info">
             <p>We install many types of wood flooring options.  They include but aren't limited to:</p>
+            <div className="sub">
             <ul>
             	<li>Prefinished Tongue and Groove</li>
             	<li>Un-finished Tongue and Groove</li>
@@ -29,6 +37,9 @@ const Floors = () => {
             </ul>
             </div>
             <div className="img">
+            </div>
+            </div>
+            </div>
             </div>
         </div>
     )
