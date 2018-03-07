38acbe435a07c5e35c6c0dc1385292762269f42f
Daniel Levenson <daniellevenson4@gmail.com>
2018-03-07 12:00:31 -0500

    set up restoration component layout

--- src/components/Restoration.jsx
+++ src/components/Restoration.jsx
@@ -1,12 +1,32 @@
 import React from 'react'
-
-// create slideshow with resto1 and 2
+import Slideshow1 from './imgs/restoration/restoration_1.JPG'
+import Slideshow2 from './imgs/restoration/restoration_2.JPG'
 
 const Restoration = () => {
     return(
-        <div>
-            <h1>Restoration Work</h1>
+        <div className="resto-container">
+            <h1 className="resto-header">Restoration Work</h1>
+            <div className="resto-subcont">
+            <div className="resto-info">
             <p>Worcester County Carpentry work with historic and craftsman style buildings to restore, or recreate, architectual details original to the home.  We also recreate certain details custom to a customer's vision.</p>
+            </div>
+            <div className="row" id="flooring-slideshow">
+            <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+            <ol className="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+			    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>        
+            </ol>
+            <div className="carousel-inner" role="listbox" id="floor-slideshow">
+                <div className="carousel-item active">
+                    <img className="d-block img-fluid" src={Slideshow1} alt="First slide" id="floor-img" />
+                </div>
+                <div className="carousel-item">
+                    <img className="d-block img-fluid" src={Slideshow2} alt="Second slide" id="floor-img" />
+                </div>
+            </div>
+            </div>
+            </div>
+            </div>
         </div>
     )
 }
