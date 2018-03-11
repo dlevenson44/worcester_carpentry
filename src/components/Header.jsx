81804318e0ac31134762ab187cb9646206db9603
Daniel Levenson <daniellevenson4@gmail.com>
2018-03-10 22:24:20 -0500

    added styling to links in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,9 +7,9 @@ const Header = () => {
         <img className="header-img" src={Logo} />
         <div className="header-subcont">
         <ul className="header-list">
-            <li><a href="tel:508-523-6797">508-523-6797</a></li>
+            <li><a href="tel:508-523-6797" id="tel">508-523-6797</a></li>
             <li>Worcester County and the surrounding areas</li>
-            <li>Need masonry work done?  <a href="http://worcesterchimneys.com/" target="_blank">Click here!</a></li>
+            <li>Need masonry work done?  <a href="http://worcesterchimneys.com/" id="mason" target="_blank">Click here!</a></li>
         </ul>
         </div>
         </div>
