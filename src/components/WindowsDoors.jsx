26c8471363819b7221e5515e36da9c8c1d23ffab
Daniel Levenson <daniellevenson4@gmail.com>
2018-03-11 17:49:05 -0400

    styled windowsdoors component

--- src/components/WindowsDoors.jsx
+++ src/components/WindowsDoors.jsx
@@ -6,11 +6,11 @@ import Slideshow3 from './imgs/windowsdoors/windowsdoors1.jpg'
 const WindowsDoors = () => {
     return(
         <div className="veneer" id="windows1">
-            <h4>We install replacement windows and new contruction windows depending on the specific application.</h4>
-            <h4>We work with many manufacturers of windows, as well as many different styles of windows.</h4>
+            <h3 className="content-h3">We install replacement windows and new contruction windows depending on the specific application.</h3>
+            <h3 className="content-h3">We work with many manufacturers of windows, as well as many different styles of windows.</h3>
             <div className="content-subcont" id="windows">
             <div className="windows-info">
-            <p>We have experience in installing doors of all types and styles also.  Doors we install include:
+            <p className="resto-para">We have experience in installing doors of all types and styles also.  Doors we install include:
             <ul>
             	<li>Entry Doors</li>
             	<li>Entry Doors with Sidelights</li>
@@ -29,13 +29,13 @@ const WindowsDoors = () => {
             </ol>
             <div className="carousel-inner" role="listbox" id="floor-slideshow">
                 <div className="carousel-item active">
-                    <img className="d-block img-fluid" src={Slideshow1} alt="First slide" id="floor-img" />
+                    <img className="d-block img-fluid" src={Slideshow1} alt="First slide" id="roof-img" />
                 </div>
                 <div className="carousel-item">
-                    <img className="d-block img-fluid" src={Slideshow2} alt="Second slide" id="floor-img" />
+                    <img className="d-block img-fluid" src={Slideshow2} alt="Second slide" id="roof-img" />
                 </div>
                 <div className="carousel-item">
-                    <img className="d-block img-fluid" src={Slideshow3} alt="Third slide" id="floor-img" />
+                    <img className="d-block img-fluid" src={Slideshow3} alt="Third slide" id="roof-img" />
                 </div>
             </div>
             </div>
