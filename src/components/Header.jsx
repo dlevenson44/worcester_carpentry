96cd2a9c735aed73ec5a0e3ec07f1f4c51e4ab14
Daniel Levenson <daniellevenson4@gmail.com>
2018-03-10 22:16:38 -0500

    added logo and fixed alignment of header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Logo from './imgs/logo.jpg'
 
 const Header = () => {
     return(
         <div className="header-container">
-        <h1 className="header">hello</h1>
+        <img className="header-img" src={Logo} />
         <div className="header-subcont">
-        <ul>
+        <ul className="header-list">
             <li><a href="tel:508-523-6797">508-523-6797</a></li>
             <li>Worcester County and the surrounding areas</li>
             <li>Need masonry work done?  <a href="http://worcesterchimneys.com/" target="_blank">Click here!</a></li>
