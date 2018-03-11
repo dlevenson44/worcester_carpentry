9bbd5e9794bec99abdd09c5175db727d19e23ec3
Daniel Levenson <daniellevenson4@gmail.com>
2018-03-11 17:51:26 -0400

    bringing home component in, set up home route

--- src/App.js
+++ src/App.js
@@ -7,17 +7,13 @@ import Decks from './components/Decks'
 import Floors from './components/Floors'
 import Gutters from './components/Gutters'
 import Header from './components/Header'
-// import Home from './components/Home'
-// import KitchenBath from './components/KitchenBath'
+import Home from './components/Home'
 import Nav from './components/Nav'
 import Remodeling from './components/Remodeling'
 import Restoration from './components/Restoration'
 import Roofing from './components/Roofing'
 import Siding from './components/Siding'
 import WindowsDoors from './components/WindowsDoors'
-
-// Route below is removed until client wants it readded
-// <Route path='/kitchenbath' component={KitchenBath}/>
 
 class App extends Component {
   render() {
@@ -26,6 +22,7 @@ class App extends Component {
         <div className="container">
           <Header />
           <Nav />
+          <Route exact path="/" component={Home} />
           <Route path='/contact' component={Contact} />
           <Route path='/decks' component={Decks}/>
           <Route path='/floors' component={Floors}/>
