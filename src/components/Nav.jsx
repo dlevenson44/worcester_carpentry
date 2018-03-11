68d619ea8ec444bc2fa2c68ab1f7d7752c8333dd
Daniel Levenson <daniellevenson4@gmail.com>
2018-03-10 23:06:42 -0500

    added nav bar styling

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -12,45 +12,45 @@ const Nav = () => {
             <div className="hidden-md-down" id="nav-container">
                 <ul className="nav justify-content-flex-start">
                 <li className="nav-item" id="nav-li">                    
-                    <Link to="/" className="nav link">Home</Link>
+                    <Link to="/" className="nav link" id="nav-spec">Home</Link>
                 </li>
 
                 <li className="nav-item" id="nav-li">                    
-                    <Link to="/decks" className="nav link">Decks</Link>
+                    <Link to="/decks" className="nav link" id="nav-spec">Decks</Link>
                 </li>
 
                 <li className="nav-item" id="nav-li">                    
-                    <Link to="/floors" className="nav link">Floors</Link>
+                    <Link to="/floors" className="nav link" id="nav-spec">Floors</Link>
                 </li>
 
                 <li className="nav-item" id="nav-li">                    
-                    <Link to="/gutters" className="nav link">Gutters</Link>
+                    <Link to="/gutters" className="nav link" id="nav-spec">Gutters</Link>
                 </li>
 
              
 
                 <li className="nav-item" id="nav-li">                    
-                    <Link to="/remodeling" className="nav link">Remodeling</Link>
+                    <Link to="/remodeling" className="nav link" id="nav-spec">Remodeling</Link>
                 </li>
 
                 <li className="nav-item" id="nav-li">                    
-                    <Link to="/restoration" className="nav link">Restoration</Link>
+                    <Link to="/restoration" className="nav link" id="nav-spec">Restoration</Link>
                 </li>                                              
 
                 <li className="nav-item" id="nav-li">                    
-                    <Link to="/roofing" className="nav link">Roofing</Link>
+                    <Link to="/roofing" className="nav link" id="nav-spec">Roofing</Link>
                 </li>
 
                 <li className="nav-item" id="nav-li">                    
-                    <Link to="/siding" className="nav link">Siding</Link>
+                    <Link to="/siding" className="nav link" id="nav-spec">Siding</Link>
                 </li>
 
                 <li className="nav-item" id="nav-li">                    
-                    <Link to="/windowsdoors" className="nav link">Windows & Doors</Link>
+                    <Link to="/windowsdoors" className="nav link" id="nav-spec">Windows & Doors</Link>
                 </li>
 
                 <li className="nav-item" id="nav-li">
-                    <Link to="/contact" className="nav link">Contact Us</Link>
+                    <Link to="/contact" className="nav link" id="nav-spec">Contact Us</Link>
                 </li>
 
                 </ul>
