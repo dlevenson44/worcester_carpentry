87c2503a60c1dd00fdabf0a1f8d1c4c8aa44ee99
Daniel Levenson <daniellevenson4@gmail.com>
2018-03-11 17:04:59 -0400

    fixed header fonts

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,9 +7,9 @@ const Header = () => {
         <img className="header-img" src={Logo} />
         <div className="header-subcont">
         <ul className="header-list">
-            <li><a href="tel:508-523-6797" id="tel">508-523-6797</a></li>
-            <li>Worcester County and the surrounding areas</li>
-            <li>Need masonry work done?  <a href="http://worcesterchimneys.com/" id="mason" target="_blank">Click here!</a></li>
+            <li className="list-header"><a href="tel:508-523-6797" id="tel">508-523-6797</a></li>
+            <li className="list-header">Worcester County and the surrounding areas</li>
+            <li className="list-header">Need masonry work done?  <a href="http://worcesterchimneys.com/" id="mason" target="_blank">Click here!</a></li>
         </ul>
         </div>
         </div>
