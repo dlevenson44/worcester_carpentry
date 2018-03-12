fff331ff6065c173debfad08f599987a9df356ac
Daniel Levenson <daniellevenson4@gmail.com>
2018-03-12 12:40:51 -0400

    added content to contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,9 +3,28 @@ import React from 'react'
 const Contact = () => {
     return(
         <div className="veneer">
-            <h1>contact info here yall</h1>
+            <h3 className="content-h3">Contact Us Today!</h3>
+            <p className="contact-info">If you are looking for expert carpentry work and maintenance, call Worcester County Carpentry today.</p>
+            <div className="content-container-r">
+            <div className="content-container-c">
+                <h5 className="sub-header">Business Hours:</h5>
+                <p className="sub-info">Monday - Saturday:</p>
+                <p className="sub-info">Call Pat today to schedule your appointment!</p>
+                <p className="sub-info">8:00 AM - 6:00 PM</p>
+            </div>
+            <div className="content-container-c">
+                <h5 className="sub-header">Phone Number:</h5>
+                <a href="tel:15085236797" className="sub-link">1-508-523-6797</a>
+                <h5 className="sub-header">Email:</h5>
+                <a href="mailto:patrick@worcesterchimneys.com" className="sub-link">patrick@worcesterchimneys.com</a>
+                <h5 className="sub-header">Address:</h5>
+                <p className="sub-info">Worcester County Carpentry</p>
+                <p className="sub-info">95 Prescott St.</p>
+                <p className="sub-info">Worcester, MA, 01605</p>                
+            </div>
+            </div>
         </div>
     )
 }
 
-export default Contact
+export default Contact;
