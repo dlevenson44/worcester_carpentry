4e22ce0b2cd9967c748a5226d111b641d754d48e
Daniel Levenson <daniellevenson4@gmail.com>
2018-03-13 18:06:06 -0400

    set up home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,14 @@ import Slideshow3 from './imgs/decksporches/decksporches1.jpg'
 
 const Home = () => {
     return (
-        <div className="veneer" id="home">
+        <div className="veneer" id="home1">
+        <div className="content-container-r" id="home1">        
+            <div id="home">
+                <h3 className="content-h3">Over 15 Years Experience</h3>
+                <p className="resto-para">Next time you're thinking of adding that new deck,</p>                
+                <p className="resto-para">restoring those scuffed floors, or remodeling your front door,</p>
+                <p className="resto-para"><a href="tel:15085236797" className="sub-link">call us</a> to get the job done right.</p>
+            </div>
             <div className="row" id="flooring-slideshow">
             <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
             <ol className="carousel-indicators">
@@ -26,6 +33,7 @@ const Home = () => {
             </div>
             </div>
             </div>
+        </div>
         </div>
     )
 }
